eb70f4c7a03fec38ef8fabeb598c9ef1d35e28f4
Dylan Main <34387758+maindk@users.noreply.github.com>
2026-04-01 14:16:20 -0400

    Update ExtractMaterial_AssignTextureMaps.cs

--- ExtractMaterial_AssignTextureMaps.cs
+++ ExtractMaterial_AssignTextureMaps.cs
@@ -1,1 +1,39 @@
+using System.IO;
+using System.Linq;
+using UnityEditor;
+using UNityEngine;
 
+public class ExtractMaterial_AssignTextureMaps : EditorWindow
+{
+  [MenuItem("Tools/ExtractMaterial_AssignTextureMaps")]
+  public static void showWIndow()
+  {
+    GetWindow<ExtractMaterial_AssignTextureMaps>("Material Extractor and Assigner);
+  }
+
+  Private void OnGUI()
+  {
+    GUILayout.Label("FBX Tool", EditorStyles.boldLabel);
+
+    string selectedName = Selected.activeObject != null ? Selection.activeObject.name : "None";
+
+    EditorGUILayout.LabelField("Currently Selected", selectedName);
+    EditorGUILayout.SelectedLabel("Copyable ID:   " + selectedName);
+
+    EditorGUILayout.Space(30);
+
+    if(GUILayout.Button("Extract Materials"))
+    {
+      ExtractMAterials();
+    }
+
+    EditorGUILayout.Space(30);
+    
+    if(GUILayout.Button("Assign TExtureMaps"))
+    {
+      AssignTextures();
+    }
+  }
+  
+  
+}
